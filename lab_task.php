def1bbb59c44b93ce3d4962db4a4e080639a33e2
tasnova taseen <23-51382-1@student.aiub.edu>
2025-11-02 17:58:35 +0600

    lab task 1 doing

---
+++ lab_task.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<head>
+    <title>lab task 1 </title>
+</head>
+
+<body>
+    <center>
+        <table>
+        <h1 style="color:blue">Clinic patient registration</h1>
+<tr>
+    <td>Full name</td>
+    <td><input type="text"></td></tr>
+
+
+    <tr>
+    <td>Age</td>
+    <td><input type="text"></td></tr>
+
+    <tr>
+    <td>Phone number</td>
+    <td><input type="text"></td></tr>
+
+    <tr>
+    <td>Email Address</td>
+    <td><input type="text"></td></tr>
+
+    <tr>
+    
+    <td>Insurance provider</td>
+
+    <
+    <td><select>
+        <option>select provider</option>
+        <option>blc</option>
+        <option>pubali</option>
+</select>
+</td>
+
+
+
+</tr>
+    
+    <tr>
+    <td>Insurance policy number</td>
+    <td><input type="text"></td></tr>
+    <tr>
+    <td><h2 style="color:blue">Additional information</h2></td></tr>
+
+    <tr>
+    <td>username</td>
+    <td><input type="text"></td></tr>
+    <tr>
+    <td>password</td>
+    <td><input type="text"></td></tr>
+    <tr>
+    <td>Confrim password</td>
+    <td><input type="text"></td></tr>
+
+
+    <tr>
+    
+   <td> <input type="Submit"></td>
+</tr>
+
+    
+
+    
+
+</center>
+</table>
+</body>
